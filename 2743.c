a034d60c7d26e7fbebabe4612fb5da6f66b0c211
hwajinstar <154287147+hwajinstar@users.noreply.github.com>
2024-10-21 09:46:21 +0900

    Create 2743.c

---
+++ 2743.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  char a[101];
+  scanf("%s",a);
+  int count = 0;
+  for (int i = 0; a[i] != '\0';i++){
+    count++;
+  }
+  printf("%d", count);
+  return 0;
+}
